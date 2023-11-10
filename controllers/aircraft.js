29d342af31a059a26f9cc5b9912496be7b83edbc
pikachu1806 <s565881@nwmissouri.edu>
2023-11-10 01:14:14 -0600

    modified post

--- controllers/aircraft.js
+++ controllers/aircraft.js
@@ -83,3 +83,24 @@ exports.aircraft_detail = async function (req, res) {
         res.send(`{"error": document for id ${req.params.id} not found`);
     }
 };
+
+// Handle aircraft update form on PUT.
+exports.aircraft_update_put = async function (req, res) {
+    console.log(`update on id ${req.params.id} with body
+    ${JSON.stringify(req.body)}`)
+    try {
+        let toUpdate = await aircraft.findById(req.params.id)
+        // Do updates of properties
+        if (req.body.aircraft_type)
+            toUpdate.aircraft_type = req.body.aircraft_type;
+        if (req.body.aircraft_model) toUpdate.aircraft_model = req.body.aircraft_model;
+        if (req.body.manufacture_year) toUpdate.manufacture_year = req.body.manufacture_year;
+        let result = await toUpdate.save();
+        console.log("Sucess " + result)
+        res.send(result)
+    } catch (err) {
+        console.log('Came to error')
+        res.status(500)
+        res.send(`{"error": "Values are not Valid failed"}`);
+    }
+};
